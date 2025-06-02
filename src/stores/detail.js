f79125e49322a7b695dba3470800ab29af566590
Miot <imuy@foxmail.com>
2025-06-02 23:12:08 +0800

    fix: 图片路径

--- src/stores/detail.js
+++ src/stores/detail.js
@@ -1,38 +1,39 @@
+const imgPath = (img) => { const url = location.origin + location.pathname; return url + img; };
 
 const textures = {
     oak: {
         id: 'oak',
         name: '橡木面板',
         texture: {
-            map: '/textures/oak_veneer_01_diff_1k.jpg',
+            map: imgPath('/textures/oak_veneer_01_diff_1k.jpg'),
         }
     },
     plywood: {
         id: 'plywood',
         name: '合成木板',
         texture: {
-            map: '/textures/plywood_diff_1k.jpg',
+            map: imgPath('/textures/plywood_diff_1k.jpg'),
         }
     },
     rosewood: {
         id: 'rosewood',
         name: '红木面板',
         texture: {
-            map: '/textures/rosewood_veneer1_diff_1k.jpg',
+            map: imgPath('/textures/rosewood_veneer1_diff_1k.jpg'),
         }
     },
     carbon: {
         id: 'carbon',
         name: '碳纤维面板',
         texture: {
-            map: '/textures/carbon-fiber_metallic.png',
+            map: imgPath('/textures/carbon-fiber_metallic.png'),
         }
     },
     walnut: {
         id: 'walnut',
         name: '胡桃木面板',
         texture: {
-            map: '/textures/walnut_diff_1k.jpg',
+            map: imgPath('/textures/walnut_diff_1k.jpg'),
         }
     },
     
