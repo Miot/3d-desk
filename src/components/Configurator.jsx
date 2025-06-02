bf7602d92f9b593992ac8195395594a01440e72b
Miot <imuy@foxmail.com>
2025-06-02 19:37:57 +0800

    feat: 电动升降按钮

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -62,11 +62,12 @@ const Configurator = () => {
                 rounded-3xl 
                 cursor-pointer 
                 ${
-                  deskConfig.frameType === item.id
-                    ? "bg-zinc-300"
-                    : "bg-white"
+                  deskConfig.frameType === item.id ? "bg-zinc-300" : "bg-white"
                 }`}
-              onClick={() => deskConfig.setFrameType(item.id)}
+              onClick={() => {
+                deskConfig.setFrameType(item.id);
+                deskConfig.setElectric(item.id == "adjustable");
+              }}
             >
               <div className="font-semibold">{item.name}</div>
             </button>
@@ -99,12 +100,43 @@ const Configurator = () => {
                  }`}
               onClick={() => deskConfig.setFrameColor(item.id)}
             >
-              <div className="h-14 w-14 rounded-full" style={{backgroundColor: item.id}}></div>
+              <div
+                className="h-14 w-14 rounded-full"
+                style={{ backgroundColor: item.id }}
+              ></div>
               <div>{item.name}</div>
             </button>
           );
         })}
       </div>
+      {deskConfig.frameType == "adjustable" && (
+        <>
+          <h2 className="font-semibold text-xl pl-2">电动升降</h2>
+          <hr />
+          <div className="flex flex-row">
+            <button
+              className={`
+            flex 
+            items-center 
+            border-2 
+            space-x-2 
+            w-full 
+            hover:bg-zinc-300 
+            transtition-all
+              p-2 
+              mt-4 
+              rounded-3xl 
+              cursor-pointer 
+              ${deskConfig.electric ? "bg-zinc-300" : "bg-white"}`}
+              onClick={() => deskConfig.setElectric(!deskConfig.electric)}
+            >
+              <div className="font-semibold">
+                {deskConfig.electric ? "已选" : "未选"}
+              </div>
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/Desk.jsx
+++ src/components/Desk.jsx
@@ -41,16 +41,16 @@ const Desk = () => {
                 <meshStandardMaterial metalness={.3} roughness={.1} color={deskConfig.frameColor} />
             </mesh>
             {/* 升降面板 */}
-            <mesh geometry={nodes.Panel.geometry} scale={[1, 1, 1]} position={nodes.Panel.position}>
-                <meshStandardMaterial metalness={.3} roughness={.1} />
+            <mesh geometry={nodes.Panel.geometry} scale={[1, 1, 1]} position={nodes.Panel.position} visible={deskConfig.electric}>
+                <meshStandardMaterial metalness={.3} roughness={.1} color={deskConfig.frameColor} />
             </mesh>
             {/* 线缆 */}
-            <mesh geometry={nodes.Wires.geometry} scale={[1, 1, 1]} position={nodes.Wires.position}>
-                <meshStandardMaterial metalness={.3} roughness={.1} />
+            <mesh geometry={nodes.Wires.geometry} scale={[1, 1, 1]} position={nodes.Wires.position} visible={deskConfig.electric}>
+                <meshStandardMaterial metalness={.3} roughness={.1} color={deskConfig.frameColor} />
             </mesh>
             {/* 横梁 */}
-            <mesh geometry={nodes.CrossBeam.geometry} scale={[1, 1, 1]} position={nodes.CrossBeam.position}>
-                <meshStandardMaterial metalness={.3} roughness={.1} />
+            <mesh geometry={nodes.CrossBeam.geometry} scale={[1, 1, 1]} position={nodes.CrossBeam.position} visible={deskConfig.frameType == 'adjustable'}>
+                <meshStandardMaterial metalness={.3} roughness={.1} color={deskConfig.frameColor} />
             </mesh>
         </group>
     )
